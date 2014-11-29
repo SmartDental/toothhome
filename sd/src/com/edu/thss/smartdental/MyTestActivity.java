e232dca23a753436abf020007f7051815d4acf8a
GengZhengLin <gzl8042@163.com>
2014-11-29 22:22:19 +0800

    add calendar

--- sd/src/com/edu/thss/smartdental/MyTestActivity.java
+++ sd/src/com/edu/thss/smartdental/MyTestActivity.java
@@ -1,39 +1,61 @@
 package com.edu.thss.smartdental;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import android.content.Intent;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MyTestActivity extends Activity {
-	Button test;  //声明控件
-	
-	public static final int RESULT_CODE = 1; //返回码
-
+	Button test;
+	public static final int RESULT_CODE = 1;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_my_test);  //和XML文件映射
-		test = (Button)findViewById(R.id.test_btn); //id号和XML中定义的ID对应
-		//test.setText("我是一个button"); //设置控件
-		
-		
-		String data = getIntent().getExtras().getString("data");
-	    test.setText(data);
-	    
+		setContentView(R.layout.activity_my_test1);
+		test = (Button)findViewById(R.id.test_button);
+		test.setText("test btn");
 		test.setOnClickListener(new OnClickListener(){
 			
 			@Override
-			public void onClick(View v) {
+			public void onClick(View v){
 				Intent intent = new Intent();
-				intent.putExtra("result-data","test");
+				intent.putExtra("result-data",test.getText() );
 				MyTestActivity.this.setResult(RESULT_CODE, intent);
+				finish();
 			}});
 		
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.my_test, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- sd/src/com/edu/thss/smartdental/Toothhome.java
+++ sd/src/com/edu/thss/smartdental/Toothhome.java
@@ -3,11 +3,18 @@ package com.edu.thss.smartdental;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
 
 public class Toothhome extends Fragment {
 	
@@ -29,10 +36,19 @@ public class Toothhome extends Fragment {
 
 			@Override
 			public void onClick(View arg0) {
-				
-				Intent intent = new Intent(getActivity(),MyTestActivity.class);
-				intent.putExtra("data",ok.getText() );
-				startActivityForResult(intent,REQUEST_CODE);
+				Fragment fragment  = new AppointmentFragment();
+				if(fragment != null){
+					FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+					fragmentManager.beginTransaction().replace(R.id.content_frame,fragment).commit();
+					
+//					mDrawerList.setItemChecked(position, true);
+//					mDrawerList.setSelection(position);
+//					setTitle(mNavMenuTitles[position]);
+//					mDrawerLayout.closeDrawer(mDrawerList);
+				}
+				else{
+					Log.e("MainActivity", "Error in creating fragment");
+				}
 			}});
 		
 		//intent.putExtra("data",ok.getText() );
@@ -41,7 +57,7 @@ public class Toothhome extends Fragment {
 	
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode == REQUEST_CODE&&resultCode == MyTestActivity.RESULT_CODE){
+		if(requestCode == REQUEST_CODE && resultCode == MyTestActivity.RESULT_CODE){
 			String result = data.getExtras().getString("result-data");
 		}
 	}
